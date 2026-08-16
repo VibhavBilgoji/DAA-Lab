f2843f5fde5713660824e4a57eebad4fce9a17ab
Vibhav Bilgoji <vibhavbilgoji@gmail.com>
2026-08-17 05:15:08 +0530

    Add .gitignore, make small changes to file1.py

--- Expt1/file1.py
+++ Expt1/file1.py
@@ -2,7 +2,7 @@ import math
 
 # a = [None, -72, -31, 71, -95, -68, -4, -55, 85, -93, 51]
 
-def MinMax(i, j, max, min):
+def MinMax(i: int, j: int, max: list[int], min: list[int]):
     if i == j:
         max[0] = min[0] = a[i]
     elif i == j-1:
@@ -15,7 +15,7 @@ def MinMax(i, j, max, min):
     else:
         mid = math.floor((i+j) / 2)
         MinMax(i, mid, max, min)
-        max1, min1 = [None], [None]
+        max1, min1 = [0], [0]
         MinMax(mid+1, j, max1, min1)
 
         if max1[0] > max[0]:
@@ -23,14 +23,14 @@ def MinMax(i, j, max, min):
         if min1[0] < min[0]:
             min[0] = min1[0]
 
-    print(i, "\t", j, "\t", max[0], "\t", min[0]);
+    print(f"{i} \t {j} \t {max[0]} \t {min[0]}")
 
 a = [0]
-print("Enter elements seperated by spaces: ");
-a.extend(int(n) for n in input().split(" "))
+print("Enter elements seperated by spaces: ")
+a.extend(int(n) for n in input().split())
 
-max, min = [None], [None]
-print("\ni\t j\tMax\tMin");
+max, min = [0], [0]
+print("\ni\t j\t Max\t Min");
 MinMax(1, len(a)-1, max, min)
 
 print("\nFinal Max:", max[0])
